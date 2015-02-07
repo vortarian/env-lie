6b5ba36656f1361dcb48ad5b0fedf68679706841
vortarian <vortarian@systemicai.com>
2015-02-07 01:57:54 -0700

    Demonstrating that the env variable is overridden in sub processes

--- server.js
+++ server.js
@@ -14,9 +14,27 @@ if(process.env.NODE_ENV in env_map) {
 }
 process.env.NODE_ENV = env;
 
-var config = require('config');
+var cluster = require('cluster');
 
 // Load all the clean-room app code below here, pass in configuration and no one else has to deal with it
-var app = require('./app.js');
+var appWorkers = {};
 
-app.run(config);
+function spawnAppWorker() {
+  var worker = cluster.fork({type: 'worker'});
+  appWorkers[worker.process.pid] = worker;
+  return worker;
+}
+
+if (cluster.isMaster) {
+  var appProcs = 1;
+  spawnAppWorker();
+  cluster.on('exit', function (worker, code) {
+  process.exit();
+  });
+} else if (process.env.type === 'worker') {
+  var config = require('config');
+  console.log('Starting app worker..');
+  require('./app').run(config);
+  process.exit();
+}
+
